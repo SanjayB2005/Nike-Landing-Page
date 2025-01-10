fd6ad9d3ca7e24f5412cc3537bb109732f1dcce7
SanjayB2005 <bsanjay2595@gmail.com>
2025-01-10 13:38:13 +0530

    Refactor Navbar and Hero section; update favicon path, improve layout, and add ShoeCards component for dynamic shoe selection

--- nike/src/Components/Navbar.jsx
+++ nike/src/Components/Navbar.jsx
@@ -5,18 +5,22 @@ import { hamburger } from '../assets/icons'
 const Navbar = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
-      <nav className='flex justify-between items-center max-container'>
+      <nav className='flex justify-between items-center  max-container'>
         <a href='/'>
           <img src={headerLogo} alt='Logo' width={130} height={30}/>
         </a>
-        <ul className='flex justify-center items-center gap-16 max-lg:hidden '>
+        <ul className='flex  flex-1 justify-center items-center gap-16 max-lg:hidden '>
          {navLinks.map((item) => (
           <li key={item.label}>
             <a href={item.href} className='font-montserrat leading-normal text-slate-gray text-lg'>{item.label}</a>
           </li>
          ))}
-         <li key="log-in"><a href='#log-in' className='font-montserrat leading-normal text-black text-lg'>Log in / Explore now</a></li>
+         
         </ul>
+        <div key="log-in" className='font-montserrat leading-normal text-black text-lg gap-2 max-lg:hidden wide:mr-24 font-medium'>
+            <a href='#sign-in' >Sign in / Explore now</a>
+        
+        </div>
         <div className='max-lg:block hidden'>
           <img src={hamburger} alt='Hamburger' width={24} height={24}/>
         </div>
