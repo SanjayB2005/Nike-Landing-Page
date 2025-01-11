33ab8a31e6eea8ff2e146d05f0bdc0aacd8d8a75
SanjayB2005 <bsanjay2595@gmail.com>
2025-01-11 17:12:34 +0530

    Enhance button and services card components; add dynamic styling options and implement SpecialOffers section with promotional content and buttons

--- nike/src/Buttons/Button.jsx
+++ nike/src/Buttons/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Button = ({name,iconUrl}) => {
+const Button = ({name,iconUrl,backgroundColor, borderColor, textColor}) => {
   return (
-    <button className='flex justify-center items-center gap-2 bg-coral-red text-white px-4 py-2 rounded-full border font-montserrat border-coral-red'> 
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 rounded-full border font-montserrat  
+                       ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}`: "bg-coral-red text-white border-coral-red"  } `}> 
         {name}
         {iconUrl && <img src={iconUrl} alt="iconUrl" className='ml-5 rounded-full w-5 h-5' />}
     </button>
